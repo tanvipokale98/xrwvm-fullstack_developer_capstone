823d9037dce17a5ab2d5ef67bcba9d77ea34f961
Tanvi <pokaletanvi095@gmail.com>
2025-10-27 12:37:41 -0400

    final

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -46,4 +46,4 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-        
+  
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -17,7 +17,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     `https://tanvipokale2-8000.
-    theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai``
+    theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai`
 ]
 
 CSRF_TRUSTED_ORIGINS = [
